10f7630080697b749cec29619f8df746977362c9
ahochman <ahochman2019@gmail.com>
2019-05-03 17:15:41 -0400

    more api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,9 +2,19 @@ var router = require("express").Router();
 var connection = require("../db/connection");
 
 router.get("/api/notes", (req, res) => {
-  connection.query("SELECT * FROM notes", (err, dbNotes) => {
-    res.json(dbNotes);
+  connection.query("SELECT * FROM notes", (err, databaseNotes) => {
+    res.json(databaseNotes);
   });
 });
+
+router.post("/api/notes", (req, res) => {
+connection.query ("INSERT INTO notes SET?". req.body, (err, results) => {
+  if (err) throw err;
+  res.json(results);
+});
+
+});
+
+
 
 module.exports = router;
